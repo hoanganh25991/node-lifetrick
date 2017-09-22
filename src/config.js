6ca3a9ed36ab38329862d5c3a8c9bad244e9a3f6
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-23 02:37:45 +0700

    Increase timeout, see it fail a lot

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ var exports = module.exports = {}
 
 const puppeteer = {
   launch: {
-    timeout: 3000,
+    timeout: 30000,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   },
   waitForSelector: {
-    timeout: 3000
+    timeout: 30000
   }
 }
 
