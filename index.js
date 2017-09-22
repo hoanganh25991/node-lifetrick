181c55abe895997e61fe2955e33a6646e02d0166
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-22 10:31:08 +0700

    Fail to try on simple real link

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const puppeteer = require('puppeteer');
+// const lifeTrickPage = "http://lifetricks.com/";
+const lifeTrickPage = "https://tinker.press/tinypos/";
 
 (async () => {
   try{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('file:///D:/work-station/node-lifetrick/index.html');
-
-    const posts  = await page.evaluate(() => {
-      return {width: window.innerWidth, height: window.innerHeight};
-      // const wrappers = document.querySelectorAll('div.reddit-post-wrapper');
-      //
-      // const posts = wrappers.map(wrapper => {
-      //  // Try to find imgUrl
-      //  const post = wrapper.querySelector('div.reddit-post')
-      //  const img = post.querySelector('img');
-      //  // Try to get the category
-      //  const postFooter = wrapper.querySelector('div.reddit-post-footer')
-      //  const anchorCategory = postFooter.querySelector('a.category-tag')
-      //  const category = anchorCategory.innerText
-      //
-      //  return {
-      //    imgUrl: img.src,
-      //    category,
-      //  }
-      // })
-      //
-      // return posts;
-    });
-
-    console.log(posts);
-    //await page.screenshot({path: 'receipt.png'});
-    browser.close();
+    await page.goto(lifeTrickPage);
+    await page.screenshot({path: 'example.png'});
+    await browser.close();
   }catch(err){
     console.log(err)
   }
