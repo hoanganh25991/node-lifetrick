b7fd9c7778b536506caeaf8f264f5e9ee8b88477
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-22 13:50:48 +0700

    Regex pattern to parse page

--- src/findPageLinkList.js
+++ src/findPageLinkList.js
@@ -2,11 +2,15 @@ const puppeteer = require('puppeteer');
 const url = "http://lifetricks.com";
 
 const waitForlinkList = (async() => {
+  console.log(new Date().getTime())
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  console.log(new Date().getTime())
   try {
+    console.log(new Date().getTime())
     const page = await browser.newPage();
     await page.goto(url);
     await page.waitForSelector('img')
+    console.log(new Date().getTime())
     /**
      * @see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageevaluatepagefunction-args
      * page.eveluate allow pass args into pageFunction
@@ -46,11 +50,38 @@ const waitForlinkList = (async() => {
     // console.log(waitForlinkList);
     return linkList;
   } catch(err) {
-    console.log(err)
+    console.log(err.message)
     return null;
   } finally {
     await browser.close();
   }
 })();
 
-waitForlinkList.then(linkList => console.log(linkList))
+const parseLinkList = (link) => {
+  console.log(new Date().getTime())
+  // Only check with current pattern
+  // If fail should die immediately
+  const pattern = /\/page\/\d+/gi;
+  const match = link.match(pattern)
+  if(match){
+    const pageX = match[0];
+    const number = pageX.match(/\d+/gi)
+    if(number){
+      console.log(number[0])
+      return number[0];
+    }
+    return null;
+  }else{
+    console.log("Die immediately. pattern not work")
+    return null;
+  }
+}
+
+waitForlinkList.then(linkList => {
+  if(linkList){
+    console.log(linkList)
+    const x = linkList.map(link => parseLinkList(link))
+    console.log(x)
+  }
+  console.log("No linkList to handle")
+}).catch(err => console.log(err))
