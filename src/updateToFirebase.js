c60305fae113449866537e0ac6d90237e95ab0e1
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-22 19:30:26 +0700

    Remove secret file from source

--- src/updateToFirebase.js
+++ src/updateToFirebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 // Fetch the service account key JSON file contents
-const serviceAccount = require("./.credential/glass-turbine-148103-firebase-adminsdk-n0gsz-0503c65cb3.json");
+const serviceAccount = require("./.credential/glass-turbine-148103-firebase-adminsdk-n0gsz-f4c7be2350.json");
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
