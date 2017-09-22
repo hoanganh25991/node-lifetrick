e46d1bd547fe98170fa1e7914a7704b4823e8a62
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-22 10:44:48 +0700

    Not good without sandbox, but let run first

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const lifeTrickPage = "https://tinker.press/tinypos/";
 
 (async () => {
   try{
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(lifeTrickPage);
     await page.screenshot({path: 'example.png'});
